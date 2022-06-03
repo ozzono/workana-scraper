494d0eb6cd692203591d5587e5027cad7b86a4f0
Hugo Virgilio <huvirgilio@gmail.com>
2022-06-03 01:13:23 -0300

    found element of interest

--- example/example.go
+++ example/example.go
@@ -8,28 +8,59 @@ import (
 )
 
 const (
-	pagesDomain = "https://www.workana.com/jobs?category=it-programming"
-	jobsDomain  = "https://www.workana.com/job/"
+	pagePrefix = "https://www.workana.com/jobs?category=it-programming&language=en"
+	jobsPrefix = "https://www.workana.com/job/"
+	tagPrefix  = "https://www.workana.com/jobs?skills="
 )
 
+type scrape struct {
+	jobs  []job
+	pages map[string]struct{}
+}
+
+type job struct {
+	url  string
+	tags []string
+}
+
 func main() {
-	c := colly.NewCollector(
-	// colly.AllowedDomains("workana.com/"),
-	)
-	// c.Async = true
+	c := colly.NewCollector()
+	scrape := scrape{[]job{}, map[string]struct{}{}}
+
 	// Find and visit all links
-	urlList := []string{}
+	stop := false
 	c.OnHTML("a", func(e *colly.HTMLElement) {
 		href := e.Attr("href")
-		if strings.HasPrefix(href, pagesDomain) || strings.HasPrefix(href, jobsDomain) {
-			e.Request.Visit(e.Attr("href"))
-			urlList = append(urlList, href)
+		if strings.HasPrefix(href, pagePrefix) && !stop {
+			stop = true
+			_, exist := scrape.pages[href]
+			if !exist {
+				scrape.pages[href] = struct{}{}
+			}
+			e.Request.Visit(href)
 		}
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+	c.OnRequest(func(r *colly.Request) {})
 
 	c.Visit("https://www.workana.com/jobs?category=it-programming&language=en")
+	scrape.GetJobs()
+}
+
+func (s *scrape) GetJobs() {
+	for key := range s.pages {
+		fmt.Println(getTags(key))
+		break
+	}
+}
+
+func getTags(url string) []string {
+	c := colly.NewCollector()
+	fmt.Println(url)
+	out := []string{}
+	c.OnHTML("[class='project-item  js-project']", func(e *colly.HTMLElement) {
+		fmt.Printf("%#v\n", e)
+	})
+	c.Visit(url)
+	return out
 }
